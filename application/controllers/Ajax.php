b1f6d255e540a7046e6741679a153d11c2940efb
Vpsolo <1foreverfarever@gmail.com>
2019-05-07 18:48:12 +0600

    1

--- application/controllers/Ajax.php
+++ application/controllers/Ajax.php
@@ -10,6 +10,13 @@ class Ajax extends CI_Controller {
 		$data['query_category'] = $this->Get_model->id_category($id_category);
 		// $data['id_category'] =$id_category;
 		$this->load->view('ajax/category_option_view', $data);
+	}	
+	public function asdasd($id_category)
+	{	
+		$this->load->model('Get_model');
+		$data['query_category'] = $this->Get_model->id_category($id_category);
+		// $data['id_category'] =$id_category;
+		$this->load->view('ajax/category_option_view', $data);
 	}
 
 }
