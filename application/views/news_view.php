b64112cb7d6b314877acbaa989d6eb7f95d95396
beki123456 <ergeshov.bekteir@mail.ru>
2019-05-08 17:19:27 +0600

    css менен иштедим

--- application/views/news_view.php
+++ application/views/news_view.php
@@ -17,9 +17,10 @@ left: 0;"></div>
 			<div class="row justify-content-center">
 				<div class="col-sm-10 col-md-8 col-lg-4 p-b-40">
 					<div class="bg-0 h-full">
-						<a href="news-detail.html" class="hov-img0 of-hidden">
-							<img src="<?php echo base_url().'assets/images/c_genadii_vyenko_1_0.jpg';?>" alt="IMG">
-						</a>
+						<a href="news-detail.html" class="hov-img0 of-hidden ">
+			
+<div class="beki" style="background-image: url('<?php echo base_url().'assets/images/c_genadii_vyenko_1_0.jpg';?>');"></div>
+				</a>
 
 						<div class="bg-0 p-rl-28 p-t-26 p-b-35">
 							<h4 class="p-b-12">
@@ -60,7 +61,8 @@ left: 0;"></div>
 				<div class="col-sm-10 col-md-8 col-lg-4 p-b-40">
 					<div class="bg-0 h-full">
 						<a href="news-detail.html" class="hov-img0 of-hidden">
-							<img src="<?php echo base_url().'assets/images/k1.jpg';?>" alt="IMG">
+							<!-- <img src="<?php echo base_url().'assets/images/k1.jpg';?>" alt="IMG"> -->
+							<div class="beki" style="background-image: url('<?php echo base_url().'assets/images/k1.jpg';?>');"></div>
 						</a>
 
 						<div class="bg-0 p-rl-28 p-t-26 p-b-35">
@@ -102,7 +104,9 @@ left: 0;"></div>
 				<div class="col-sm-10 col-md-8 col-lg-4 p-b-40">
 					<div class="bg-0 h-full">
 						<a href="news-detail.html" class="hov-img0 of-hidden">
-							<img src="<?php echo base_url().'assets/images/k2.jpg';?>" alt="IMG">
+					<!-- 		<img src="<?php echo base_url().'assets/images/k2.jpg';?>" alt="IMG"> -->
+							<div class="beki" style="background-image: url('<?php echo base_url().'assets/images/k2.jpg';?>');"></div>
+
 						</a>
 
 						<div class="bg-0 p-rl-28 p-t-26 p-b-35">
