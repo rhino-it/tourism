f4e90601360796000f5c9115cf98bb2863d96591
beki123456 <ergeshov.bekteir@mail.ru>
2019-05-14 18:19:40 +0600

    жаны страница ачтым

---
+++ application/views/about_view.php
@@ -0,0 +1,492 @@
+<!-- Title page -->
+	<section class="bg-img1 kit-overlay1" style="background-image: url(images/bg-05.jpg);">
+		<div class="container size-h-3 p-tb-30 flex-col-c-c">
+			<h2 class="t1-b-1 text-uppercase cl-0 txt-center m-b-25">
+				About Us
+			</h2>
+
+			<div class="flex-wr-c-c">
+				<a href="index.html" class="breadcrumb-item">
+					Home 
+				</a>
+
+				<span class="breadcrumb-item">
+					About Us
+				</span>
+			</div>
+		</div>
+	</section>
+
+	<!--  -->
+	<section class="bg-0 p-t-92 p-b-60">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-sm-10 col-md-6 p-b-35">
+					<!-- Title section -->
+					<div class="flex-col-c-s p-b-35">
+						<h3 class="t1-b-1 cl-3 m-b-11">
+							Mission & Vision
+						</h3>
+
+						<div class="size-a-2 bg-3"></div>
+					</div>
+
+					<div class="p-r-20 p-r-0-sr767">
+						<p class="t1-s-2 cl-6 m-b-10">
+							<span class="t1-s-5 cl-3">At present:</span> Leave your information and email address. We will call back and advise you. Duis aute irure dolor in reprehenderit in voluptate. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere offis possimus 
+						</p>
+
+						<p class="t1-s-2 cl-6 m-b-10">
+							<span class="t1-s-5 cl-3">In the future:</span> Leave your information and email address. We will call back and advise you. Duis aute irure dolor in reprehenderit in voluptate. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus.
+						</p>
+					</div>
+				</div>
+
+				<div class="col-sm-10 col-md-6 p-b-35">
+					<!-- Title section -->
+					<div class="flex-col-c-s p-b-8">
+						<h3 class="t1-b-1 cl-3 m-b-11">
+							Our Stats
+						</h3>
+
+						<div class="size-a-2 bg-3"></div>
+					</div>
+
+					<!-- Progress -->
+					<div class="wrap-progress size-w-6 w-full-sr767">
+						<span class="dis-block t1-s-4 cl-3 p-b-8 p-t-26">
+							Financial Analyst
+						</span>
+
+						<div class="progress-item">
+							<div class="progress-inner txt-s-113 cl0 txt-right p-r-10" data-percent="93%"></div>
+						</div>
+
+						<span class="dis-block t1-s-4 cl-3 p-b-8 p-t-26">
+							Business Suports
+						</span>
+
+						<div class="progress-item">
+							<div class="progress-inner txt-s-113 cl0 txt-right p-r-10" data-percent="60%"></div>
+						</div>
+
+						<span class="dis-block t1-s-4 cl-3 p-b-8 p-t-26">
+							Inverstments
+						</span>
+
+						<div class="progress-item">
+							<div class="progress-inner txt-s-113 cl0 txt-right p-r-10" data-percent="80%"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Why chosse us -->
+	<section class="bg-12 p-t-92 p-b-70">
+		<div class="container">
+			<!-- Title section -->
+			<div class="flex-col-c-c p-b-50">
+				<h3 class="t1-b-1 cl-3 txt-center m-b-11">
+					Why Chosse Us
+				</h3>
+
+				<div class="size-a-2 bg-3"></div>
+			</div>
+
+			<!--  -->
+			<div class="row justify-content-center">
+				<div class="col-sm-10 col-md-6 col-lg-3 p-b-30">
+					<!-- Block1 -->
+					<div class="block1 trans-04">
+						<div class="block1-show trans-04">
+							<div class="block1-symbol txt-center wrap-pic-max-s m-b-23 pos-relative lh-00 trans-04">
+								<img class="symbol-dark trans-04" src="images/icons/symbol-01-dark.png" alt="IMG">
+								<img class="symbol-light ab-t-c op-00 trans-04" src="images/icons/symbol-01-light.png" alt="IMG">
+							</div>
+
+							<h4 class="block1-title t1-m-1 text-uppercase cl-3 txt-center trans-04">
+								Experienced adviser
+							</h4>
+						</div>
+							
+						<div class="block1-hide flex-col-c-c p-t-8 trans-04">
+							<p class="t1-s-2 cl-12 txt-center p-b-26">
+								Sed nec egestas diam, vitae imper-diet nisi. Vivamus cursus
+							</p>
+
+							<a href="#" class="flex-c-c size-a-1 p-rl-15 t1-s-2 text-uppercase cl-6 bg-0 hov-btn1 trans-02">
+								Read More
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-10 col-md-6 col-lg-3 p-b-30">
+					<!-- Block1 -->
+					<div class="block1 trans-04">
+						<div class="block1-show trans-04">
+							<div class="block1-symbol txt-center wrap-pic-max-s m-b-23 pos-relative lh-00 trans-04">
+								<img class="symbol-dark trans-04" src="images/icons/symbol-02-dark.png" alt="IMG">
+								<img class="symbol-light ab-t-c op-00 trans-04" src="images/icons/symbol-02-light.png" alt="IMG">
+							</div>
+
+							<h4 class="block1-title t1-m-1 text-uppercase cl-3 txt-center trans-04">
+								Great Ideas 
+							</h4>
+						</div>
+							
+						<div class="block1-hide flex-col-c-c p-t-8 trans-04">
+							<p class="t1-s-2 cl-12 txt-center p-b-26">
+								Sed nec egestas diam, vitae imper-diet nisi. Vivamus cursus
+							</p>
+
+							<a href="#" class="flex-c-c size-a-1 p-rl-15 t1-s-2 text-uppercase cl-6 bg-0 hov-btn1 trans-02">
+								Read More
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-10 col-md-6 col-lg-3 p-b-30">
+					<!-- Block1 -->
+					<div class="block1 trans-04">
+						<div class="block1-show trans-04">
+							<div class="block1-symbol txt-center wrap-pic-max-s m-b-23 pos-relative lh-00 trans-04">
+								<img class="symbol-dark trans-04" src="images/icons/symbol-03-dark.png" alt="IMG">
+								<img class="symbol-light ab-t-c op-00 trans-04" src="images/icons/symbol-03-light.png" alt="IMG">
+							</div>
+
+							<h4 class="block1-title t1-m-1 text-uppercase cl-3 txt-center trans-04">
+								Worldwide System
+							</h4>
+						</div>
+							
+						<div class="block1-hide flex-col-c-c p-t-8 trans-04">
+							<p class="t1-s-2 cl-12 txt-center p-b-26">
+								Sed nec egestas diam, vitae imper-diet nisi. Vivamus cursus
+							</p>
+
+							<a href="#" class="flex-c-c size-a-1 p-rl-15 t1-s-2 text-uppercase cl-6 bg-0 hov-btn1 trans-02">
+								Read More
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-10 col-md-6 col-lg-3 p-b-30">
+					<!-- Block1 -->
+					<div class="block1 trans-04">
+						<div class="block1-show trans-04">
+							<div class="block1-symbol txt-center wrap-pic-max-s m-b-23 pos-relative lh-00 trans-04">
+								<img class="symbol-dark trans-04" src="images/icons/symbol-04-dark.png" alt="IMG">
+								<img class="symbol-light ab-t-c op-00 trans-04" src="images/icons/symbol-04-light.png" alt="IMG">
+							</div>
+
+							<h4 class="block1-title t1-m-1 text-uppercase cl-3 txt-center trans-04">
+								Security Information
+							</h4>
+						</div>
+							
+						<div class="block1-hide flex-col-c-c p-t-8 trans-04">
+							<p class="t1-s-2 cl-12 txt-center p-b-26">
+								Sed nec egestas diam, vitae imper-diet nisi. Vivamus cursus
+							</p>
+
+							<a href="#" class="flex-c-c size-a-1 p-rl-15 t1-s-2 text-uppercase cl-6 bg-0 hov-btn1 trans-02">
+								Read More
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Number -->
+	<div class="parallax100 kit-overlay1 p-tb-38" style="background-image: url(images/bg-01.jpg);">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 p-tb-30">
+					<div class="flex-col-c-c h-full">
+						<span class="t1-b-2 cl-0 txt-center p-b-5 counter">
+							450
+						</span>
+
+						<span class="t1-m-1 cl-13 txt-center text-uppercase">
+							Projects 
+						</span>
+					</div>
+				</div>
+
+				<div class="col-md-4 p-tb-30">
+					<div class="flex-col-c-c h-full">
+						<span class="t1-b-2 cl-0 txt-center p-b-5 counter">
+							205
+						</span>
+
+						<span class="t1-m-1 cl-13 txt-center text-uppercase">
+							advisor
+						</span>
+					</div>
+				</div>
+
+				<div class="col-md-4 p-tb-30">
+					<div class="flex-col-c-c h-full">
+						<span class="t1-b-2 cl-0 txt-center p-b-5">
+							<span class="counter">95</span>%
+						</span>
+
+						<span class="t1-m-1 cl-13 txt-center text-uppercase">
+							satisfaction rate
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Team -->
+	<section class="bg-0 p-t-92 p-b-52">
+		<div class="container">
+			<!-- Title section -->
+			<div class="flex-col-c-c p-b-50">
+				<h3 class="t1-b-1 cl-3 txt-center m-b-11">
+					Meet Our Team
+				</h3>
+
+				<div class="size-a-2 bg-3"></div>
+			</div>
+
+			<!--  -->
+			<div class="row justify-content-center">
+				<div class="col-sm-6 col-md-5 col-lg-3 p-b-40">
+					<div>
+						<div class="wrap-pic-w pos-relative">
+							<img src="images/team-01.jpg" alt="IMG">
+
+							<div class="s-full ab-t-l flex-wr-c-c p-tb-30 hov-2">
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-facebook"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-twitter"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-google-plus"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-instagram"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-linkedin"></i>	
+								</a>
+							</div>
+						</div>
+
+						<div class="flex-col-c-c p-t-28">
+							<a href="#" class="t1-m-1 text-uppercase cl-3 txt-center hov-link2 trans-02 m-b-5">
+								Jessie Garcia
+							</a>
+
+							<span class="t1-s-5 cl-6 txt-center">
+								Sales Manager
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-md-5 col-lg-3 p-b-40">
+					<div>
+						<div class="wrap-pic-w pos-relative">
+							<img src="images/team-02.jpg" alt="IMG">
+
+							<div class="s-full ab-t-l flex-wr-c-c p-tb-30 hov-2">
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-facebook"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-twitter"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-google-plus"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-instagram"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-linkedin"></i>	
+								</a>
+							</div>
+						</div>
+
+						<div class="flex-col-c-c p-t-28">
+							<a href="#" class="t1-m-1 text-uppercase cl-3 txt-center hov-link2 trans-02 m-b-5">
+								Charles Sims
+							</a>
+
+							<span class="t1-s-5 cl-6 txt-center">
+								CEO Company
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-md-5 col-lg-3 p-b-40">
+					<div>
+						<div class="wrap-pic-w pos-relative">
+							<img src="images/team-03.jpg" alt="IMG">
+
+							<div class="s-full ab-t-l flex-wr-c-c p-tb-30 hov-2">
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-facebook"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-twitter"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-google-plus"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-instagram"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-linkedin"></i>	
+								</a>
+							</div>
+						</div>
+
+						<div class="flex-col-c-c p-t-28">
+							<a href="#" class="t1-m-1 text-uppercase cl-3 txt-center hov-link2 trans-02 m-b-5">
+								Angel Meyer
+							</a>
+
+							<span class="t1-s-5 cl-6 txt-center">
+								Supervisor
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-md-5 col-lg-3 p-b-40">
+					<div>
+						<div class="wrap-pic-w pos-relative">
+							<img src="images/team-04.jpg" alt="IMG">
+
+							<div class="s-full ab-t-l flex-wr-c-c p-tb-30 hov-2">
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-facebook"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-twitter"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-google-plus"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-instagram"></i>	
+								</a>
+
+								<a href="#" class="flex-c-c size-a-7 borad-50per bg-0 fs-16 cl-11 hov-btn3 m-all-5 hov-2-1">
+									<i class="fa fa-linkedin"></i>	
+								</a>
+							</div>
+						</div>
+
+						<div class="flex-col-c-c p-t-28">
+							<a href="#" class="t1-m-1 text-uppercase cl-3 txt-center hov-link2 trans-02 m-b-5">
+								Ron Harper
+							</a>
+
+							<span class="t1-s-5 cl-6 txt-center">
+								Representative
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Testimonials -->
+	<section class="bg-12 p-t-92 p-b-70">
+		<div class="container">
+			<!-- Title section -->
+			<div class="flex-col-c-c p-b-50">
+				<h3 class="t1-b-1 cl-3 txt-center m-b-11">
+					Testimonials
+				</h3>
+
+				<div class="size-a-2 bg-3"></div>
+			</div>
+
+			<!--  -->
+			<div class="row justify-content-center">
+				<div class="col-sm-10 col-md-11 col-lg-6 p-b-30 wrap-block3">
+					<div class="block3 d-flex rs1-block3">
+						<div class="block3-content d-flex">
+							<div class="block3-pic wrap-pic-w">
+								<img src="images/ava-01.jpg" alt="IMG">
+							</div>
+
+							<div class="block3-text d-flex w-full-sr575">
+								<span class="block3-text-child t1-m-1 text-uppercase cl-3 p-b-4">
+									Marie Crawford
+								</span>
+
+								<span class="block3-text-child t1-s-3 cl-7 p-b-9">
+									Wall Street Combany
+								</span>
+
+								<p class="block3-text-child t1-s-2 cl-6">
+									The point of using Lorem Ipsum is that it has a normal distr bution of letters, as opposed to using Many desktop publis shing packages and web page Ipsum.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-10 col-md-11 col-lg-6 p-b-30 wrap-block3">
+					<div class="block3 d-flex rs1-block3">
+						<div class="block3-content d-flex">
+							<div class="block3-pic wrap-pic-w">
+								<img src="images/ava-02.jpg" alt="IMG">
+							</div>
+
+							<div class="block3-text d-flex w-full-sr575">
+								<span class="block3-text-child t1-m-1 text-uppercase cl-3 p-b-4">
+									Jerry Alexander
+								</span>
+
+								<span class="block3-text-child t1-s-3 cl-7 p-b-9">
+									Wall Street Combany
+								</span>
+
+								<p class="block3-text-child t1-s-2 cl-6">
+									The point of using Lorem Ipsum is that it has a normal distr bution of letters, as opposed to using Many desktop publis shing packages and web page Ipsum.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+	</section>
