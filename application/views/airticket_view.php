2c5e6f4a3470fa45a2279f2500d9ef550544c665
rhino-it <it.academy.osh@gmail.com>
2019-05-17 20:15:07 +0600

    добавил animate.css и сделал страницы

--- application/views/airticket_view.php
+++ application/views/airticket_view.php
@@ -1,5 +1,28 @@
 	<!-- Airticket -->
-	<section class="bg-0 p-t-40 p-b-60" style="position: relative;">
+	  <section class="bg-img1 kit-overlay1" style="background-image: url(<?php echo base_url('assets/images/ticket2.jpg');?>);">
+
+
+		<div class="container size-h-3 p-tb-30 flex-col-c-c">
+			<h2 class="t1-b-1 text-uppercase cl-0 txt-center m-b-25">
+				 Авиабилеты
+			</h2>
+			<div class="flex-wr-c-c">
+				<a href="<?php echo base_url(); ?>" class="breadcrumb-item">
+					Главная 
+				</a>
+
+				<span class="breadcrumb-item">
+					Авиабилеты
+				</span>
+			</div>
+		</div>
+	</section>
+
+
+
+
+
+	<!-- <section class="bg-0 p-t-40 p-b-60" style="position: relative;">
 		<div class="container">
 			
 			<div class="k_aviabilety">АВИАБИЛЕТЫ</div>
@@ -16,12 +39,64 @@
 				<div class="k_plane" style="background-image: url(<?php echo base_url('assets/images/planepng2.png');?>);"></div>
 			</div>
 		</div>
+	</section> -->
+
+
+	
+
+<section class="bg-0 p-t-92 p-b-60">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-sm-12 col-md-12 p-b-35">
+					<!-- Title section -->
+					<div class="flex-col-c-s p-b-35">
+						<h3 class="t1-b-1 cl-3 m-b-11">
+							Круглосуточное бронирование билетов по WhatsApp 
+						</h3>
+
+						<div class="size-a-2 bg-3"></div>
+					</div>
+
+					<div class="p-r-20 p-r-0-sr767">
+						<p class="t1-s-2 cl-6 m-b-10">
+							Напишите нам в приложении WhatsApp на номер +996 (774) 35 37 44 куда и когда хотите полететь. Мы найдем оптимальные маршруты для вас в любое время суток.
+						</p>
+
+						<a class="wow pulse k_whatsApp_button"  data-wow-duration="1s" data-wow-iteration ="2";  href="https://api.whatsapp.com/send?phone=996551336944&text=Здравствуйте,%20pamir.kg!%20Хочу%20забронировать%20авиабилет">
+					Написать в WhatsApp +996 (774) 35 37 44</a>
+
+						<p class="t1-s-2 cl-6 m-b-10">
+							Оплату можно произвести у нас в офисе или через Электронный кошелек «ЭЛСОМ» по номеру 0774 35 37 44.
+						</p>
+
+
+						<!-- <p class="t1-s-2 cl-6 m-b-10">
+							<span class="t1-s-5 cl-3">Наше турагенство в Оше</span> специализируются на 
+						</p> -->
+					</div>
+				</div>
+
+	
+			</div>
+		</div>
 	</section>
+
+
+
+
 	<section>
 		<div class="container">
-			<div class="k_partners">
+			<!-- <div class="k_partners">
 				<h2>АВИАБИЛЕТЫ ОТ АВИАКОМПАНИЙ-ПАРТНЕРОВ PAMIR.KG</h2>
-			</div>
+			</div> -->
+			<!-- Title section -->
+					<div class="flex-col-c-s p-b-35">
+						<h3 class="t1-b-0 cl-3 m-b-11">
+							Авиабилеты от Авиакомпаний-партнеров pamir.kg
+						</h3>
+
+						<div class="size-a-2 bg-3"></div>
+					</div>
 			<div class="row">
 				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
 					<div class="k_partners_block" >
--- application/views/head_view.php
+++ application/views/head_view.php
@@ -32,6 +32,12 @@
 <!--===============================================================================================-->	
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/main.css';?>">
 <!--===============================================================================================-->
+
+<script src="<?php echo base_url().'assets/js/wow.min.js';?>"></script>
+<script>
+		new WOW().init();
+	</script>
 </head>
+
 
 <body class="animsition">
